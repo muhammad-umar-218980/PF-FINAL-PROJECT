e888af49ef327ecefcd91116f7767495b8b0acad
muhammad-umar-218980 <muhammad.umar.siddique.1502@gmail.com>
2024-12-17 01:33:19 +0500

    updated
    errors removed

--- Hangman_Game.c
+++ Hangman_Game.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
-#include <ctype.h>
-#include <windows.h>
+#include <stdlib.h>  
+#include <string.h> 
+#include <time.h>   
+#include <ctype.h>  
+#include <windows.h>  
 
-// <------------------------------------------ FUNCTION PROTOTYPES ------------------------------------------>
-void animation();
+// <------------------------------ FUNCTION PROTOTYPES ------------------------------->
+void animation(); //same
 
-void welcomeAgain();
+void welcomeAgain(); //same
 
 void drawHangman(int guesses);
 
@@ -22,24 +22,23 @@ void clearScreen() {
     system("cls");
 }
 
-void byeAnimation();
+void byeAnimation(); //same
 
-int score = 0;  // jab round khatam ho jayega score again 0 ho jayega is lie global variable set kia hai 
+int score = 0;  
+// <--------------------------------- MAIN FUNCTION ------------------------------------>
 
-// <------------------------------------------ MAIN FUNCTION ------------------------------------------>
 int main() {
     srand(time(NULL)); 
-    char word[20]; 
+    char word[20]; //yei string hai
     char choice;
 
-    animation();
-
+    animation(); 
     do {
-        int category = chooseCategory();
+        int category = chooseCategory(); //267
         getRandomWord(word, category);
         playRound(word);
-
-        do {
+   
+do {
             printf("\nDo you want to play again? (Y/N): ");
             scanf(" %c", &choice);
             choice = tolower(choice);
@@ -60,11 +59,9 @@ int main() {
     return 0;
 }
 
-// <------------------------------------------ FUNCTION DEFINITIONS ------------------------------------------>
+// <------------------------------ FUNCTION DEFINITIONS ------------------------------->
 
-
-
-// <------------------------------------------ ANIMATION ------------------------------------------>
+// <------------------------------------ ANIMATION ------------------------------------------>
 
 void animation() {
     char text[] = "WELCOME TO HANGMAN (The Ultimate Word Guessing Game)";
@@ -83,7 +80,8 @@ void animation() {
         Sleep(100);
     }
 
-    printf("\n\n");
+    
+printf("\n\n");
 
     printf("          ");
     for (int i = 0; i < length + 30; i++) {
@@ -93,12 +91,11 @@ void animation() {
 }
 
 
+// <--------------------------- WELCOME AGAIN ANIMATION ---------------------------->
 
-
-
-// <------------------------------------------ WELCOME AGAIN ANIMATION ------------------------------------------>
 void welcomeAgain() {
-    char text[] = "WELCOME AGAIN !!";
+   
+ char text[] = "WELCOME AGAIN !!";
     int length = strlen(text);
 
     printf("          ");
@@ -127,10 +124,11 @@ void welcomeAgain() {
 
 
 
+// <--------------------------- BYE AGAIN ANIMATION ------------------------------->
 
-// <------------------------------------------ BYE AGAIN ANIMATION ------------------------------------------>
 void byeAnimation(){
-    char text[] = "\n\n THANK YOU FOR PLAYING HANGMAN \n\n GOOD BYE !!";
+   
+ char text[] = "\n\n THANK YOU FOR PLAYING HANGMAN \n\n GOOD BYE !!";
     int length = strlen(text);
     
     for (int i = 0; text[i] != '\0'; i++) {
@@ -142,11 +140,7 @@ void byeAnimation(){
 }
 
 
-
-
-
-// <------------------------------------------ DRAW HANGMAN FUNCTION------------------------------------------>
-// ye function hangman draw karega guesses k according 
+// <------------------------------ DRAW HANGMAN FUNCTION---------------------------->
 
 void drawHangman(int guesses) {
     switch (guesses) {
@@ -210,6 +204,12 @@ void drawHangman(int guesses) {
             printf("\n=========\n");
             break;
 
+        
+
+
+
+       
+
         case 0:
             printf("\n +-----+ ");
             printf("\n |     | ");
@@ -222,12 +222,8 @@ void drawHangman(int guesses) {
     }
 }
 
+// <----------------------------- CHOOSE CATEGORY---------------------------------->
 
-
-
-
-// <------------------------------------------ CHOOSE CATEGORY------------------------------------------>
-// so basically this function is used to choose a category out of 4 
 int chooseCategory() {
     printf("\nSelect a category:\n");
     printf("1. Cities\n");
@@ -247,17 +243,16 @@ int chooseCategory() {
     return choice;
 }
 
+// <-------------------------- RANDOM WORD PICKER FUNCTION----------------------->
 
-
-
-// <------------------------------------------ RANDOM WORD PICKER FUNCTION------------------------------------------>
-// <------------------------------------------ FILE HANDLING FUNCTION------------------------------------------>
-//ye function file se random word select karega
+// <----------------------------- FILE HANDLING FUNCTION-------------------------------->
 void getRandomWord(char word[], int category) {
     char fileName[20];
-    FILE *file;
+  
+  FILE *file; 
 
-    if (category == 1) {
+
+if (category == 1) {
         strcpy(fileName, "cities.txt");
     } else if (category == 2) {
         strcpy(fileName, "countries.txt");
@@ -267,60 +262,57 @@ void getRandomWord(char word[], int category) {
         strcpy(fileName, "vegetables.txt");
     } else {
         printf("Invalid category selected.\n");
+        exit(1); 
+    }
+
+    file = fopen(fileName, "r");  
+   
+ if (file == NULL) {   
+        printf("Error: Could not open file %s\n", fileName);  
+        
         exit(1);
     }
 
-    file = fopen(fileName, "r");
-    if (file == NULL) {
-        printf("Error: Could not open file %s\n", fileName);
-        exit(1);
-    }
-
-    char words[30][20];
+    char words[30][20];  
     int count = 0;
     char temp[20];
 
-    while (fgets(temp, sizeof(temp), file)) {
-        temp[strcspn(temp, "\n")] = '\0';
-        strcpy(words[count], temp);
+    while (fgets(temp, sizeof(temp), file)) {  
+        
+         temp[strcspn(temp, "\n")] = '\0';  
+       
+         strcpy(words[count], temp); 
         count++;
     }
 
     fclose(file);
 
-//    if (count == 0) {
-//        printf("Error: No words found in file %s\n", fileName);
-//        exit(1);
-//    }
-
-    int randomIndex = rand() % count;
-    strcpy(word, words[randomIndex]);
+    int randomIndex = rand() % count; 
+    
+    strcpy(word, words[randomIndex]); 
 }
 
 
 
 
-
-// <------------------------------------------ FUNCTION FOR PLAYING HANGMAN------------------------------------------>
-// basically is function mein puray game ki logic hai 
+// <------------------------ FUNCTION FOR PLAYING HANGMAN----------------------->
 
 void playRound(char word[]) {
+   
     char guessedWord[20] = {0}; 
     char guessedLetters[26] = {0}; 
     int guessedCount = 0;
-    int guesses = 6; // Total guesses 6 hain or har ghalat guess pr ye reduce hogi by 1 upto 0 
+    int guesses = 6; 
     int wordLength = strlen(word);
     int gameWon = 0;
 
-    // Ye Guessed word ki length k equal _ _ store karega in each index
     for (int i = 0; i < wordLength; i++) {
         guessedWord[i] = '_';
     }
 
     while (guesses > 0 && !gameWon) {
         drawHangman(guesses);
-        
-        // ye Guessed words ki jagah _ _ print karega
+       
         printf("\nGuessed word: ");
         for (int i = 0; i < wordLength; i++) {
             printf("%c ", guessedWord[i]);
@@ -331,24 +323,21 @@ void playRound(char word[]) {
         for (int i = 0; i < guessedCount; i++) {
             printf("%c ", guessedLetters[i]);
         }
-        
-        
-        
         printf("\n\nGuess a letter: ");
 
         char guess;
         scanf(" %c", &guess);
         guess = tolower(guess);
 
-        // isalpha function ye check karega k user ne alphabet k ilawa tou kuch enter nhi kia
         if (!isalpha(guess)) {
-            printf("\nInvalid input! Please enter a letter.\n");
-            Sleep(1000);
+            printf("\nInvalid input! Please enter a letter.\n"); 
             continue;
         }
+     
 
-        // Ye loop check karega k agar user ne already letter guess kia hai ya nhi
-        int alreadyGuessed = 0;
+
+
+      int alreadyGuessed = 0;
         for (int i = 0; i < guessedCount; i++) {
             if (guessedLetters[i] == guess) {
                 alreadyGuessed = 1;
@@ -358,16 +347,14 @@ void playRound(char word[]) {
 
         if (alreadyGuessed) {
             printf("\nYou have already guessed the letter %c.\n", guess);
-            Sleep(1000);
             continue;
         }
 
-        // ye guessed letter store karega
         guessedLetters[guessedCount++] = guess;
 
         int correctGuess = 0;
         
-        // Ye function guessed word ko check karega k sahi hai ya nhi
+        
         for (int i = 0; i < wordLength; i++) {
             if (tolower(word[i]) == guess) {
                 guessedWord[i] = word[i];
@@ -382,7 +369,6 @@ void playRound(char word[]) {
             printf("\nIncorrect guess. You have %d guesses left.\n", guesses);
         }
 
-        // Ye function check karega k agar word completely guess ho gaya ya nhi agar nhi hua so loop will continue till guesses are 0 
         gameWon = 1;
         for (int i = 0; i < wordLength; i++) {
             if (guessedWord[i] == '_') {
@@ -392,7 +378,8 @@ void playRound(char word[]) {
         }
     }
 
-// Score calculate karnay k lie
+
+
     int roundScore = 0;
     if (guesses == 6) {
         roundScore = 100;
